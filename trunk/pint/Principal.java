ed5fc7aa69ccc375df3a866e9c02882b9add6c7a
drune <drune@e6c91e3f-a917-0410-ab7e-a08898d31f85>
2006-07-03 14:38:02 +0000

    botao

--- trunk/pint/Principal.java
+++ trunk/pint/Principal.java
@@ -1,4 +1,34 @@
+import java.awt.*;
+import java.applet.*;
+import java.awt.event.*;
 
-public class Principal {
+public class Principal 
+extends Applet implements ActionListener {
 
+	private Button botao = new Button("Mudar");
+	String zenice = "Kules Programador";
+	public void init(){
+		add(botao);
+		botao.addActionListener(this);
+	}
+	
+	public void paint(Graphics g) {
+		//g.setColor(black);
+		//g.drawLine(0,0,50,40);
+		g.drawString(zenice, 50, 50);
+	}
+	
+	public void actionPerformed(ActionEvent e){
+		if (e.getSource() == botao){
+			zenice = "Tripeiro malandro";
+			
+		}
+		repaint();
+	}
+	
+	public void changeString(String s){
+		zenice = s;
+		repaint();
+				
+	}
 }
