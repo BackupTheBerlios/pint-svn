04aa4348b47e4a6517530a070db7ed0154acd802
drune <drune@e6c91e3f-a917-0410-ab7e-a08898d31f85>
2006-07-04 16:52:25 +0000

    Redirect st's

---
+++ trunk/pint/Redirect.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.awt.*;
+import java.applet.*;
+
+public class Redirect extends OutputStream {
+
+	private TextArea textControl;
+	
+	public Redirect (TextArea control) {
+        textControl = control;
+    }
+	
+public Redirect() {super();}
+
+
+public void write( int b ) throws IOException {
+    textControl.append(String.valueOf((char)b));
+}
+
+}
