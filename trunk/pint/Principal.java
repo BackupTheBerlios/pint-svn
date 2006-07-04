68ccca0698751879edaadba764c0d5897b7b3d29
drune <drune@e6c91e3f-a917-0410-ab7e-a08898d31f85>
2006-07-04 16:53:15 +0000

    applet executes and output python code

--- trunk/pint/Principal.java
+++ trunk/pint/Principal.java
@@ -1,45 +1,64 @@
 import java.awt.*;
 import java.applet.*;
 import java.awt.event.*;
-
+import java.io.*;
 import org.python.util.PythonInterpreter;
 import org.python.core.*; 
 
 public class Principal 
 extends Applet implements ActionListener {
 
-	private Button botao = new Button("Mudar");
-	String zenice = "Kules Programador";
+	private Button botao = new Button("Executar");
+	TextField inputText = new TextField("", 30);
+	TextArea outputText = new TextArea(5, 50);
+	PythonInterpreter interp = new PythonInterpreter();
+	
+	
+	
+	
 	public void init(){
+		PrintStream out = new PrintStream( new Redirect( outputText ) );
+		
 		add(botao);
+		add(inputText);
+		add(outputText);
+
+		interp.setOut(out);
 		botao.addActionListener(this);
+		//System.setOut(out);
+		//System.setErr(out);
+		
 	}
 	
 	public void paint(Graphics g) {
-		//g.setColor(black);
-		//g.drawLine(0,0,50,40);
-		g.drawString(zenice, 50, 50);
+		// g.setColor(black);
+		// g.drawLine(0,0,50,40);
+		g.drawString("Jython Interactive Console", 5, 5);
 	}
 	
 	public void actionPerformed(ActionEvent e){
 		if (e.getSource() == botao){
-			zenice = "Tripeiro malandro";
+			
+			interpreta(inputText.getText().trim());
 			
 		}
 		repaint();
 	}
 	
-	public void changeString(String s){
-		zenice = s;
-		repaint();
-				
-	}
 	
 	public void interpreta(String s){
-		PythonInterpreter interp =
-		    new PythonInterpreter();
 		
-		interp.exec(s)
+		
+		//interp.setOut()
+		
+		s= "a=5\nb=6\nif b>a:\t print b";
+		interp.exec(s);
+		
+		// repaint
+		repaint();
+
 	}
+
+	
 	
 }
